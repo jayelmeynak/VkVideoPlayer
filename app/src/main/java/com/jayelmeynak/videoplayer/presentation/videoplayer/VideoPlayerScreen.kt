48135b89c2fde2933fd448f278cabd2f0f8ac22e
jayelmeynak <magomed110301@yandex.ru>
2025-02-22 23:01:49 +0300

    feat: Enhance video player screen with fullscreen toggle and layout adjustments

--- app/src/main/java/com/jayelmeynak/videoplayer/presentation/videoplayer/VideoPlayerScreen.kt
+++ app/src/main/java/com/jayelmeynak/videoplayer/presentation/videoplayer/VideoPlayerScreen.kt
@@ -1,14 +1,16 @@
 package com.jayelmeynak.videoplayer.presentation.videoplayer
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import android.content.res.Configuration
+import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -17,9 +19,12 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.ui.PlayerView
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
+@SuppressLint("ContextCastToActivity")
+@OptIn(UnstableApi::class)
 @Composable
 fun VideoPlayerScreen(
     videoUrl: String,
@@ -27,12 +32,13 @@ fun VideoPlayerScreen(
     viewModel: PlayerViewModel = hiltViewModel()
 ) {
     val context = LocalContext.current
+    val activity = LocalContext.current as? Activity
     val configuration = LocalConfiguration.current
     val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
     val systemUiController = rememberSystemUiController()
     val exoPlayer by viewModel.player.collectAsState()
 
-    SideEffect {
+    LaunchedEffect(isLandscape) {
         systemUiController.isNavigationBarVisible = !isLandscape
         systemUiController.isStatusBarVisible = !isLandscape
     }
@@ -52,10 +58,21 @@ fun VideoPlayerScreen(
                     PlayerView(context).apply {
                         this.player = exoPlayer
                         useController = true
+                        controllerShowTimeoutMs = 2000
+                        setFullscreenButtonClickListener {
+                            if (it) {
+                                activity?.requestedOrientation =
+                                    ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                            } else {
+                                activity?.requestedOrientation =
+                                    ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+                            }
+                        }
+                        setFullscreenButtonState(isLandscape)
                     }
                 },
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxSize()
                     .aspectRatio(if (isLandscape) configuration.screenWidthDp.toFloat() / configuration.screenHeightDp else 16f / 9f)
             )
         }
